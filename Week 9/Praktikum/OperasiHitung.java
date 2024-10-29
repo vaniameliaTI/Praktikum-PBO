a7efa217aadf54cde594388762ea588aaad35bf1
vann <vaniamelia12@students.untidar.ac.id>
2024-10-29 16:45:39 +0700

    Add files via upload

---
+++ Week 9/Praktikum/OperasiHitung.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum10;
+
+/**
+ *
+ * @author ASUS
+ */
+public interface OperasiHitung {
+    int hitung(int a, int b);
+    
+}
